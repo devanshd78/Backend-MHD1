8eeb1eb76e2362f49b4e1a561537a7d92b2150a3
devanshd78 <75110399+devanshd78@users.noreply.github.com>
2025-05-14 11:53:15 +0530

    Update Link.js

--- models/Link.js
+++ models/Link.js
@@ -1,8 +1,11 @@
-// models/Link.js
 const mongoose = require('mongoose');
+
 const linkSchema = new mongoose.Schema({
   title:     { type: String, default: 'Entry Form' },
   createdBy: { type: String, ref: 'Admin' },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  target:    { type: Number, required: true }, // New field
+  amount:    { type: Number, required: true }  // New field
 });
+
 module.exports = mongoose.model('Link', linkSchema);
